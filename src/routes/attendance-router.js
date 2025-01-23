3626ca89ea7d1f3a052df58bc2e711fc3ea2bc4f
ananthee89 <ananthee89@gmail.com>
2025-01-23 15:06:42 +0530

    cros added in markattendance
    cors added in markattendance

--- src/routes/attendance-router.js
+++ src/routes/attendance-router.js
@@ -3,8 +3,20 @@ var router = express.Router();
 const {AuthToken} = require('stashook-utils');
 const AttendanceController = require('../controller/attendance-controller')
 
+const cors =require('cors');
+var whitelist = ['http://localhost:4200']
+let corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+router.options('*',cors());
 
-router.post('/markAttendance', AuthToken.validateToken, AttendanceController.markAttendance);
+router.post('/markAttendance', cors(corsOptionsDelegate),AuthToken.validateToken, AttendanceController.markAttendance);
 
 router.post('/getMarkedAttendance',AuthToken.validateToken, AttendanceController.getMarkedAttendance);
 
